dae18daab5780d3375c7dce9219998144f8994e7
Thomas-Geraghty <thomascgeraghty@outlook.com>
2018-04-09 00:25:09 +0100

    Initial class fields

---
+++ model/User.php
@@ -0,0 +1,21 @@
+/**
+* Created by IntelliJ IDEA.
+* User: Tom
+* Date: 09/04/2018
+* Time: 00:18
+*/
+
+<?php
+
+class User {
+
+    private $username;
+    private $password;
+    private $email;
+    private $firstName;
+    private $lastName;
+    private $DOB;
+
+}
+
+?>
