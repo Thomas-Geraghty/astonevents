6cde1eb8050824a3b108ccaa23a195a579e55808
Thomas-Geraghty <thomascgeraghty@outlook.com>
2018-04-22 16:53:04 +0100

    Added generating of events table

--- php/view/EventView.php
+++ php/view/EventView.php
@@ -5,14 +5,25 @@
  * Date: 09/04/2018
  */
 
-require Config;
+require_once 'php/view/View.php';
 
-class EventView implements View {
+
+class EventView implements View
+{
 
     public function display()
     {
-        // TODO: Implement display() method.
-        Config::getDatabase()->
+        $lol = Config::getDatabase()->fetchRecord("events", "*", ['event_organiser' => $_SESSION['userID']]);
+
+        foreach ($lol as $row) {
+            echo "<tr class='eventRow' onclick='myFunction();'>
+                    <td>" . $row['event_name'] . "</td> 
+                    <td>" . $row['event_type'] . "</td>
+                    <td>" . $row['event_time'] . "</td>
+                    <td>" . $row['event_location'] . "</td>
+                </tr>
+            ";
+        }
     }
 }
 ?>
--- php/view/UserView.php
+++ php/view/UserView.php
@@ -1,9 +1,8 @@
-/**
-* Author: Tom Geraghty
-* Date: 09/04/2018
-*/
-
 <?php
+/**
+ * Author: Tom Geraghty
+ * Date: 09/04/2018
+ */
 
 class UserView implements View {
     public function display()
