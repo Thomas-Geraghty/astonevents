966ccd130c45e75f87b0d84862247704c897328c
Thomas-Geraghty <thomascgeraghty@outlook.com>
2018-04-25 01:56:42 +0100

    Refactored requires and added new functions for displaying vents.

--- php/view/EventView.php
+++ php/view/EventView.php
@@ -5,25 +5,44 @@
  * Date: 09/04/2018
  */
 
-require_once 'php/view/View.php';
+require_once ($_SERVER["DOCUMENT_ROOT"]) . '/php/Config.php';
 
+class EventView {
 
-class EventView implements View
-{
+    public static function displayEvent($eventID) {
+        $eventData = Config::getDatabase()->fetchRecord("events", "*", ['id' => $eventID])->fetch();
+        $organiserData = Config::getDatabase()->fetchRecord("users", ['first_name', 'last_name', 'email', 'phone'], ['id' => $eventData['event_organiser']])->fetch();
+        $photos = Config::getDatabase()->fetchRecord("event_photos", ['file_location'], ['event_id' => $eventID])->fetchall();
+        $array = array_merge($eventData, $organiserData);
+        $array['photos'] = $photos;
+        return $array;
+    }
 
-    public function display()
-    {
-        $lol = Config::getDatabase()->fetchRecord("events", "*", ['event_organiser' => $_SESSION['userID']]);
+    public static function displayMyEvents($userID) {
+        return Config::getDatabase()->fetchRecord("events", "*", ['event_organiser' => $userID])->fetchAll();
+    }
 
-        foreach ($lol as $row) {
-            echo "<tr class='eventRow' onclick='myFunction();'>
-                    <td>" . $row['event_name'] . "</td> 
-                    <td>" . $row['event_type'] . "</td>
-                    <td>" . $row['event_time'] . "</td>
-                    <td>" . $row['event_location'] . "</td>
-                </tr>
-            ";
-        }
+    public static function displayAllEvents() {
+        return Config::getDatabase()->fetchRecord("events", "*", [])->fetchAll();
     }
 }
+?>
+
+<?php
+if (isset($_GET['request_type'])):
+
+    if($_GET['request_type'] == 1) {
+        echo json_encode(EventView::displayAllEvents());
+    }
+    if($_GET['request_type'] == 2) {
+        if (isset($_GET['user_ID'])) {
+            echo json_encode(EventView::displayMyEvents($_GET['user_ID']));
+        }
+    }
+    if($_GET['request_type'] == 3) {
+        if (isset($_GET['event_ID'])) {
+            echo json_encode(EventView::displayEvent($_GET['event_ID']));
+        }
+    }
+endif;
 ?>
