5fa4f07d61a7c0f5b87970a829671443bcbf6dc6
Thomas-Geraghty <thomascgeraghty@outlook.com>
2018-04-25 01:51:40 +0100

    Added username taken warning

---
+++ js/Signup.js
@@ -0,0 +1,12 @@
+function liveUsernameSearch(val) {
+    $.post('php/controller/account/Signup.php', {'usernameStr': val}, function (data) {
+        if (val.length > 0) {
+            if (data == 1) {
+                $('#usernameMarker').html("<span style='color:green;'>Username is available</span>");
+            }
+            else {
+                $('#usernameMarker').html("<span style='color:red;'>Username is not available</span>")
+            }
+        }
+    })
+}
