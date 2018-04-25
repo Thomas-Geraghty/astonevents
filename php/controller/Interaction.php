955494c9b4a4388b68e062fa1a48036909a25178
Thomas-Geraghty <thomascgeraghty@outlook.com>
2018-04-25 01:54:12 +0100

    Added new sanitation methods for photos

--- php/controller/Interaction.php
+++ php/controller/Interaction.php
@@ -7,12 +7,39 @@
  */
 
 abstract class Interaction {
-    protected function sanitizeInput($data)
-    {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+    private static function sanitizeText($text) {
+        $text = trim($text);
+        $text = stripslashes($text);
+        $text = htmlspecialchars($text);
+        return $text;
+    }
+
+    private static function sanitizeImage($image) {
+        if(strstr(mime_content_type($image), 'image/')) {
+            return $image;
+        } else {
+            return null;
+        }
+    }
+
+    static function sanitizeTextInputs($whitelist, $POSTData) {
+        $sanitizedData = [];
+        foreach($whitelist as $key) {
+            if (isset($POSTData[$key])) {
+                $sanitizedData[$key] = self::sanitizeText($POSTData[$key]);
+            }
+        }
+        return $sanitizedData;
+    }
+
+    static function sanitizeImageInputs($whitelist, $POSTData) {
+        $sanitizedData = [];
+        foreach($whitelist as $key) {
+            if (isset($POSTData[$key])) {
+                $sanitizedData[$key] = self::sanitizeImage($POSTData[$key]);
+            }
+        }
+        return $sanitizedData;
     }
 }
 ?>
