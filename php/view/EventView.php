2e95856400b73cb7db251e22f02b2547006216fd
Thomas-Geraghty <thomascgeraghty@outlook.com>
2018-04-25 11:18:03 +0100

    Updated event view

--- php/view/EventView.php
+++ php/view/EventView.php
@@ -11,6 +11,10 @@ class EventView {
 
     public static function displayEvent($eventID) {
         $eventData = Config::getDatabase()->fetchRecord("events", "*", ['id' => $eventID])->fetch();
+        if($eventData == false) {
+            return false;
+        }
+
         $organiserData = Config::getDatabase()->fetchRecord("users", ['first_name', 'last_name', 'email', 'phone'], ['id' => $eventData['event_organiser']])->fetch();
         $photos = Config::getDatabase()->fetchRecord("event_photos", ['file_location'], ['event_id' => $eventID])->fetchall();
         $array = array_merge($eventData, $organiserData);
@@ -22,6 +26,10 @@ class EventView {
         return Config::getDatabase()->fetchRecord("events", "*", ['event_organiser' => $userID])->fetchAll();
     }
 
+    public static function displayCategoryEvents($category) {
+        return Config::getDatabase()->fetchRecord("events", "*", ['event_type' => $category])->fetchAll();
+    }
+
     public static function displayAllEvents() {
         return Config::getDatabase()->fetchRecord("events", "*", [])->fetchAll();
     }
@@ -30,7 +38,6 @@ class EventView {
 
 <?php
 if (isset($_GET['request_type'])):
-
     if($_GET['request_type'] == 1) {
         echo json_encode(EventView::displayAllEvents());
     }
@@ -42,6 +49,11 @@ if (isset($_GET['request_type'])):
     if($_GET['request_type'] == 3) {
         if (isset($_GET['event_ID'])) {
             echo json_encode(EventView::displayEvent($_GET['event_ID']));
+        }
+    }
+    if($_GET['request_type'] == 4) {
+        if (isset($_GET['category'])) {
+            echo json_encode(EventView::displayCategoryEvents($_GET['category']));
         }
     }
 endif;
